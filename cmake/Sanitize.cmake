e7e8f53c719379e0d296877430fd04a2a3d098da
hoyeziyu <yeziyu789@gmail.com>
2026-02-15 21:04:17 +0800

    增加ASan进行内存溢出检测

---
+++ cmake/Sanitize.cmake
@@ -0,0 +1,23 @@
+﻿cmake_minimum_required(VERSION 3.23)
+
+set(SANITIZE_CONDITION "$<CONFIG:Sanitize>")
+
+if (MSVC)
+    # 直接在全局添加标志
+    add_compile_options("$<${SANITIZE_CONDITION}:/fsanitize=address>")
+    add_compile_options("$<${SANITIZE_CONDITION}:/Zi>")
+    add_compile_options("$<${SANITIZE_CONDITION}:/Od>") # 禁用优化，防止溢出代码被优化掉
+    
+    # 链接选项
+    add_link_options("$<${SANITIZE_CONDITION}:/fsanitize=address>")
+    add_link_options("$<${SANITIZE_CONDITION}:/INCREMENTAL:NO>")
+else()
+  set(SANITIZE_FLAGS
+    "$<${SANITIZE_CONDITION}:-fsanitize=address>"
+    "$<${SANITIZE_CONDITION}:-fno-omit-frame-pointer>"
+    "$<${SANITIZE_CONDITION}:-g>"
+  )
+endif()
+
+add_compile_options(${SANITIZE_FLAGS})
+add_link_options(${SANITIZE_FLAGS})
--- greeter/src/Greeter.cpp
+++ greeter/src/Greeter.cpp
@@ -1,9 +1,12 @@
 #include <greeter/Greeter.h> // Namespaced include 
+#include <iostream>
 
 std::string get_greeting() {
 	return "Hello from the modular Greeter library!";
 }
 
 std::string Greeter::greet() const {
+	char buffer[5];
+	std::cout << buffer[5];
 	return "Hello from the Greeter class!";
 }
