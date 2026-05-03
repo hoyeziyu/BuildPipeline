e5f9cf6c59301c2e0588951ec69bca7dbf3cea53
hoyeziyu <yeziyu789@gmail.com>
2026-05-03 15:47:56 +0800

    修复测试错误提交

--- greeter/src/Greeter.cpp
+++ greeter/src/Greeter.cpp
@@ -2,10 +2,9 @@
 #include <iostream>
 #include <InternalDetails.h>
 
-// std::string get_greeting() {
-//     return "Hello from the modular Greeter library!" + InternalDetails::Name;
-// }
-
+std::string get_greeting() {
+    return "Hello from the modular Greeter library!" + InternalDetails::Name;
+}
 
 std::string Greeter::greet() const {
 	char buffer[5];
